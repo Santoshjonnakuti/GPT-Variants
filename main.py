415de3a150965bdbb6b91505986de933b114060d
Santoshjonnakuti <santoshjonnakuti@gmail.com>
2023-04-11 19:40:08 +0530

    GPT's Done

--- main.py
+++ main.py
@@ -18,21 +18,21 @@ app = Flask(__name__)
 openai.api_key = api_key
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/image-gpt', methods=['GET', 'POST'])
 def generate_image():
   if request.method == 'GET':
-    return render_template('index.html', url=None, error=False)
+    return render_template('imagegpt.html', url=None, error=False)
   else:
     prompt = request.form['input']
     if not prompt:
-      render_template('index.html', url='', error=True)
+      render_template('imagegpt.html', url='', error=True)
     # Use OpenAI to generate an image
     response = openai.Image.create(
         prompt=prompt,
         n=1,
         size="1024x1024"
     )
-    return render_template('index.html', url=response['data'][0]['url'], error=False)
+    return render_template('imagegpt.html', url=response['data'][0]['url'], error=False)
 
 @app.route("/audio-gpt", methods=["GET", "POST"])
 def audioGPT():
@@ -49,5 +49,85 @@ def audioGPT():
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     return render_template('audiogpt.html', text=transcript['text'])
   
+@app.route("/edits-gpt", methods=["GET", "POST"])
+def editsGPT():
+  if request.method == 'GET':
+    return render_template("editsgpt.html")
+  else:
+    text = request.form['text']
+    prompt = request.form['prompt']
+    print(text, prompt)
+    response = openai.Edit.create(
+      model="text-davinci-edit-001",
+      input=text,
+      instruction=prompt
+    )
+    print(response)
+    return render_template("editsgpt.html", text=response['choices'][0]['text'])
+
+@app.route("/moderations-gpt", methods=["GET", "POST"])
+def moderationsGPT():
+  if request.method == 'GET':
+    return render_template('moderationsgpt.html')
+  else:
+    text = request.form['text']
+    response = openai.Moderation.create(
+      input=text,
+    )
+    print(response)
+    return render_template('moderationsgpt.html', text=response['results'])
+  
+@app.route("/completions-gpt", methods=["GET", "POST"])
+def completionsGPT():
+  if request.method == 'GET':
+    return render_template('completionsgpt.html')
+  else:
+    text = request.form['text']
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt=text,
+      max_tokens=800,
+      temperature=0
+    )
+    print(response)
+    return render_template('completionsgpt.html', text=text + ' ' + response['choices'][0]['text'])
+
+@app.route("/audio-to-moderations-gpt", methods=["GET", "POST"])
+def audioToModerations():
+  if request.method == 'GET':
+    return render_template('audioToModerationsgpt.html')
+  else:
+    prompt = request.files['file']
+    print(prompt.filename)
+    prompt.save(prompt.filename)
+    audio_file = open(prompt.filename, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    text=transcript['text']
+    response = openai.Moderation.create(
+      input=text,
+    )
+    print(response)
+    return render_template('audioToModerationsgpt.html', text=text, table=response['results'])
+    
+@app.route("/audio-to-image-gpt", methods=["GET", "POST"])
+def audioToImage():
+  if request.method == 'GET':
+    return render_template('audioToImagegpt.html')
+  else:
+    prompt = request.files['file']
+    print(prompt.filename)
+    prompt.save(prompt.filename)
+    audio_file = open(prompt.filename, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    text=transcript['text']
+    response = openai.Image.create(
+        prompt=text,
+        n=1,
+        size="1024x1024"
+    )
+    return render_template('audioToImagegpt.html', text=text, url=response['data'][0]['url'])
+    
+    
+
 if __name__ == '__main__':
     app.run(debug=True, port=8088)
